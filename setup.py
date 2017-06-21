45f1868a13538d970c3b8d5f199ee361fc52e929
Jeroen van der Heijden <jeroen@transceptor.technology>
2017-06-21 16:03:02 +0200

    Update changelog and version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ python3 setup.py sdist upload -r pypi
 import setuptools
 from distutils.core import setup, Extension
 
-VERSION = '0.9.7'
+VERSION = '0.9.8'
 
 install_requires = [
     'aiohttp==2.2.0',
